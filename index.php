25679a799a5cf77a39b46af7b716305588c58461
KevinTeach <kevinteachings@gmail.com>
2023-01-25 09:13:41 +0700

    New Repo

---
+++ index.php
@@ -0,0 +1,159 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="asset/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Nama Sekolah</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Main Menu</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body card-glassed">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Gallery Sekolah</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Menu Tambahan
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark">
+              <li><a class="dropdown-item" href="#">Struktur Organisasi Sekolah</a></li>
+              <li><a class="dropdown-item" href="#">Tentang Sekolah</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex mt-3" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="padding"></div>
+<div class="container">
+    <br>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="https://media.istockphoto.com/id/1338737959/id/foto/anak-anak-kecil-murid-murid-berlari-bergegas-ke-gedung-sekolah-untuk-pelajaran-kelas-dari-ke.jpg?s=1024x1024&w=is&k=20&c=tuqEw6Q55FKMIHmj97JDWmhT3SQN8HF2hBTzck5Oa84=" class="d-block w-100 picture pts" alt="inside creative house">
+        </div>
+        <div class="carousel-item">
+        <img src="https://media.istockphoto.com/id/1338737952/id/foto/anak-anak-kecil-murid-murid-berlari-keluar-dari-gedung-sekolah-setelah-kelas-pelajaran-ke-bus.jpg?s=1024x1024&w=is&k=20&c=SH_jlS7BHcrxkftwY_XdtLxCinKgsZrpkjqSpdu4vkE=" class="d-block w-100 picture pts" alt="inside creative house">
+        </div>
+        <div class="carousel-item">
+        <img src="https://media.istockphoto.com/id/92207577/id/foto/guru-tk-berdiri-dengan-anak-anak-di-koridor.jpg?s=1024x1024&w=is&k=20&c=4Ab7DDrtipMox3eK7CpOp6mZlZYO3HDiUIujq27uLb8=" class="d-block w-100 picture pts" alt="inside creative house">
+        </div>
+      </div>
+    </div>
+    <br>
+    <div class="separator" data-aos="fade-up"
+     data-aos-anchor-placement="bottom-center">
+        <div class="symbold"></div>
+        <br>
+        <div class="text-center font">
+            Profil&nbsp;Sekolah
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="row align-items-start">
+        <div class="accordion" data-aos="fade-up"
+     data-aos-anchor-placement="bottom-center" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                  Kata Sambutan
+                </button>
+              </h2>
+              <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                <div class="accordion-body trasn">
+                  <strong>Point Sambutan</strong>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias porro itaque magnam, sunt doloremque voluptate aperiam quaerat neque tempora, minus numquam iste ex consectetur facere ab tempore non quo quidem?</p>
+                </div>
+              </div>
+            </div>
+            </div>
+     </div>
+     <br>
+     <br>
+     <br>
+     <br>
+    <div class="separator" data-aos="fade-up"
+     data-aos-anchor-placement="bottom-center">
+        <div class="symbold"></div>
+        <br>
+        <div class="text-center font">
+            Berita&nbsp;Sekolah
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+      <div class="row">
+          <div class="col">
+              <div class="card trasn" data-aos="fade-up" data-aos-duration="3000">
+                      <div class="card-header">
+                        <div class="float-right">
+                            <h4>Judul Berita Sekolah</h4>
+                        </div>
+                      </div>
+                      <div class="card-body text-justify">
+                          <img class="picture w-100 float-md-end mb-3 ms-md-3"src="https://media.istockphoto.com/id/92207577/id/foto/guru-tk-berdiri-dengan-anak-anak-di-koridor.jpg?s=1024x1024&w=is&k=20&c=4Ab7DDrtipMox3eK7CpOp6mZlZYO3HDiUIujq27uLb8=" alt="" srcset="">
+                          <br>
+                          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis ab perferendis, doloribus, ratione consectetur corporis accusamus exercitationem blanditiis fugit quo quibusdam quisquam, ad similique iusto animi non et hic delectus!
+                          </p>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+          <br>
+     <br>
+    <div class="separator">
+        <div class="symbold"></div>
+        <br>
+        <div class="text-center font">
+            Prestasi&nbsp;Sekolah
+        </div>
+    </div>
+        <br>
+        <br>
+        <br>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <img src="" alt="" srcset="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+</div>
+<script>
+  AOS.init();
+</script>
+</body>
+</html>
